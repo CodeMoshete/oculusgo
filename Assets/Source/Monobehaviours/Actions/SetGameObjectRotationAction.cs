52c19ad1f2968b265bc74a9c2b5ae4be2d6f09cc
Ben Mosher <codemoshete@gmail.com>
2018-09-27 15:59:31 -0700

    Add set rotation action

---
+++ Assets/Source/Monobehaviours/Actions/SetGameObjectRotationAction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SetGameObjectRotationAction : CustomAction
+{
+    public Transform Target;
+    public Vector3 EulerAngles;
+
+    public override void Initiate()
+    {
+        Target.eulerAngles = EulerAngles;
+    }
+}
