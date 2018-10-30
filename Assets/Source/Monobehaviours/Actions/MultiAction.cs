6150db3b9d5f3e72e9cee7d39ea0011bc495a937
Codemoshete <codemoshete@gmail.com>
2018-10-30 11:13:58 -0700

    Set up some more actions

---
+++ Assets/Source/Monobehaviours/Actions/MultiAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class MultiAction : CustomAction
+{
+    public List<CustomAction> NextActions;
+    public override void Initiate()
+    {
+        for (int i = 0, count = NextActions.Count; i < count; ++i)
+        {
+            NextActions[i].Initiate();
+        }
+    }
+}
