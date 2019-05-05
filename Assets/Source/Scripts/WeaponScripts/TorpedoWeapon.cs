c963f9be51c4bed0eb47cbcce3be67893de4fece
Codemoshete <codemoshete@gmail.com>
2019-05-05 16:31:57 -0700

    Add new scripts for weapons

---
+++ Assets/Source/Scripts/WeaponScripts/TorpedoWeapon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TorpedoWeapon : WeaponBase
+{
+    public GameObject Explosion;
+    private Vector3 movementDir;
+
+    public override void Fire(Vector3 target, float velocity)
+    {
+        base.Fire(target, velocity);
+
+        movementDir = Vector3.Normalize(target - transform.position) * velocity;
+    }
+
+    private void Update()
+    {
+        transform.Translate(movementDir);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(Explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Source/Scripts/WeaponScripts/WeaponBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class WeaponBase : MonoBehaviour
+{
+    protected Vector3 TargetPos;
+    protected float Velocity;
+
+    public virtual void Fire(Vector3 target, float velocity)
+    {
+        TargetPos = target;
+        Velocity = velocity;
+    }
+}
