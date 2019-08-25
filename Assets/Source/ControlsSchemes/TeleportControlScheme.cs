6bc659413188c1a845abc8137c34d644d791ede2
Ben Mosher <codemoshete@gmail.com>
2019-08-25 12:14:03 -0700

    Refactor to prep for addition of teleport movement

---
+++ Assets/Source/ControlsSchemes/TeleportControlScheme.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class TeleportControlScheme : IControlScheme
+{
+    private bool disableMovement;
+    private OVRPlayerController bodyObject;
+    private Transform cameraObject;
+    private bool hasPlayerTurned;
+    private Vector3 lastMousePos;
+    private float sensitivity;
+    private bool isDebugMenuActive;
+
+    public void Initialize(OVRPlayerController body, Transform camera, float sensitivity)
+    {
+        bodyObject = body;
+        cameraObject = camera;
+        this.sensitivity = sensitivity;
+
+        Service.Controls.SetTouchObserver(TouchUpdate);
+        Service.Controls.SetBackButtonObserver(BackUpdate);
+    }
+
+    public void Deactivate()
+    {
+        Service.Controls.RemoveTouchObserver(TouchUpdate);
+        Service.Controls.RemoveBackButtonObserver(BackUpdate);
+    }
+
+    public void SetMovementEnabled(bool enabled)
+    {
+        disableMovement = !enabled;
+    }
+
+    private void BackUpdate(BackButtonUpdate update)
+    {
+        if (update.BackButtonClicked)
+        {
+            isDebugMenuActive = !isDebugMenuActive;
+            Service.EventManager.SendEvent(EventId.DebugToggleConsole, isDebugMenuActive);
+        }
+    }
+
+    private void TouchUpdate(TouchpadUpdate update)
+    {
+        float dt = Time.deltaTime;
+
+        bool isPressed = update.TouchpadPressState;
+        bool isPressedThisFrame = update.TouchpadClicked;
+
+        if (ControlsManager.Instance.CurrentHeadset == HeadsetModel.OculusQuest)
+        {
+            OVRInput.Controller activeController = OVRInput.GetActiveController();
+        }
+
+#if UNITY_EDITOR
+        Vector3 euler = bodyObject.transform.eulerAngles;
+        if (Input.GetMouseButtonDown(1))
+        {
+            lastMousePos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector3 mouseDelta = lastMousePos - Input.mousePosition;
+            lastMousePos = Input.mousePosition;
+            euler = bodyObject.transform.eulerAngles;
+            euler.y += dt * -((mouseDelta.x / Screen.width) * sensitivity);
+            bodyObject.transform.eulerAngles = euler;
+
+            euler = cameraObject.eulerAngles;
+            euler.x += dt * (mouseDelta.y / Screen.height) * sensitivity;
+            cameraObject.eulerAngles = euler;
+        }
+#endif
+    }
+}
--- Assets/Source/Monobehaviours/Engine.cs
+++ Assets/Source/Monobehaviours/Engine.cs
@@ -1,17 +1,133 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Utils;
 
 public class Engine : MonoBehaviour
 {
+    [System.Serializable]
+    public struct PlayerCameraMirror
+    {
+        public Transform MirrorCamera;
+        public float MovementScale;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float BackgroundCameraScale = 1f;
+
+    public List<PlayerCameraMirror> MirrorCameras;
+
+    public Text ConsoleLine;
+	public float Sensitivity = 50f;
+
+    public Transform HeadCamera;
+    public float OculusGoCameraHeight;
+    public float OculusQuestCameraHeight;
+
+    private Dictionary<ControlScheme, IControlScheme> controlSchemes;
+    private IControlScheme currentControlScheme;
+
+    private Transform backgroundCamera;
+	private OVRPlayerController bodyObject;
+	private Transform cameraObject;
+    private bool isDebugMenuActive;
+
+    private Vector3 lastPlayerPosition;
+
+    private bool isJobsActive = true;
+
+	void Start ()
+    {
+		bodyObject = GameObject.Find ("OVRPlayerController").GetComponent<OVRPlayerController>();
+        for (int i = 0, count = MirrorCameras.Count; i < count; ++i)
+        {
+            float scale = MirrorCameras[i].MovementScale;
+            MirrorCameras[i].MirrorCamera.localScale = new Vector3(scale, scale, scale);
+        }
+
+		cameraObject = UnityUtils.FindGameObject(bodyObject.gameObject, "TrackingSpace").transform;
+        lastPlayerPosition = cameraObject.transform.position;
+
+        UpdateManager manager = UpdateManager.Instance;
+
+        controlSchemes = new Dictionary<ControlScheme, IControlScheme>();
+        controlSchemes.Add(ControlScheme.Movement, new MovementControlScheme());
+        SetControlScheme(ControlScheme.Movement);
+
+        Vector3 cameraStartPos = HeadCamera.localPosition;
+        switch(Service.Controls.CurrentHeadset)
+        {
+            case HeadsetModel.OculusGo:
+                cameraStartPos.y = OculusGoCameraHeight;
+                break;
+            case HeadsetModel.OculusQuest:
+                cameraStartPos.y = OculusQuestCameraHeight;
+                break;
+        }
+        HeadCamera.localPosition = cameraStartPos;
+
+        Service.EventManager.AddListener(EventId.SetControlsEnabled, OnControlsEnableSet);
+
+        Log("Device name: " + SystemInfo.deviceName + 
+            "\nDevice model: " + SystemInfo.deviceModel +
+            "\nDevice Type: " + SystemInfo.deviceType);
+    }
+
+    private void OnDestroy()
+    {
+        Service.EventManager.RemoveListener(EventId.SetControlsEnabled, OnControlsEnableSet);
+        currentControlScheme.Deactivate();
+    }
+
+    private void SetControlScheme(ControlScheme type)
+    {
+        IControlScheme nextScheme = controlSchemes[type];
+        if (nextScheme != currentControlScheme && currentControlScheme != null)
+        {
+            currentControlScheme.Deactivate();
+        }
+        currentControlScheme = nextScheme;
+        currentControlScheme.Initialize(bodyObject, cameraObject, Sensitivity);
+    }
+
+    public bool OnControlsEnableSet(object cookie)
+    {
+        currentControlScheme.SetMovementEnabled((bool)cookie);
+        return true;
+    }
+
+	void Update () 
+	{
+        float dt = Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            isDebugMenuActive = !isDebugMenuActive;
+            Service.EventManager.SendEvent(EventId.DebugToggleConsole, isDebugMenuActive);
+		}
+
+        if (isDebugMenuActive && Input.GetKeyDown(KeyCode.J))
+        {
+            isJobsActive = !isJobsActive;
+            Log("Jobs activated: " + isJobsActive);
+            Service.EventManager.SendEvent(EventId.DebugToggleJobs, isJobsActive);
+        }
+
+        for (int i = 0, count = MirrorCameras.Count; i < count; ++i)
+        {
+            float scale = MirrorCameras[i].MovementScale;
+            Vector3 moveDist = (cameraObject.transform.position - lastPlayerPosition) * scale;
+            MirrorCameras[i].MirrorCamera.localPosition += moveDist;
+            MirrorCameras[i].MirrorCamera.localRotation = cameraObject.transform.rotation;
+        }
+        lastPlayerPosition = cameraObject.transform.position;
+    }
+
+    private void Log(string message)
+    {
+        if (ConsoleLine != null)
+        {
+            ConsoleLine.text = message;
+        }
+        Debug.Log(message);
+    }
 }
