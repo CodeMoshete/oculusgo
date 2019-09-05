d71b5aee1affcc87acc9af0d2c5abab2ec44f5d4
Ben Mosher <codemoshete@gmail.com>
2019-09-04 21:18:49 -0700

    Add stubbed out piloting control scheme

---
+++ Assets/Source/ControlsSchemes/PilotingControlScheme.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotingControlScheme : IControlScheme
+{
+    private const string PILOTING_CONTAINER_NAME = "PilotingContainer";
+    private const float NAV_BOUNDS = 0.2f;
+    private Transform pilotingContainer;
+
+    public void Initialize(OVRPlayerController body, Transform camera, float sensitivity)
+    {
+        pilotingContainer = GameObject.Find(PILOTING_CONTAINER_NAME).transform;
+
+        if (pilotingContainer != null)
+        {
+            Service.Controls.SetTouchObserver(OnTouchUpdate);
+        }
+        else
+        {
+            Debug.LogError("[PilotingControlScheme] Could not find piloting container object!");
+        }
+    }
+
+    public void SetMovementEnabled(bool enabled)
+    {
+
+    }
+
+    public void Deactivate()
+    {
+
+    }
+
+    public void OnTouchUpdate(TouchpadUpdate touchUpdate)
+    {
+        
+    }
+}
