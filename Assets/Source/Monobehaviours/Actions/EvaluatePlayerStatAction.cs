4b672377c1bba568e7ea4c1671205e6449adbdd5
Codemoshete <codemoshete@gmail.com>
2019-09-28 10:38:16 -0700

    Add player stat actions

---
+++ Assets/Source/Monobehaviours/Actions/EvaluatePlayerStatAction.cs
@@ -0,0 +1,46 @@
+﻿public enum StatEvaluation
+{
+    GreaterThan,
+    LessThan,
+    EqualTo,
+    NotEqualTo
+}
+
+public class EvaluatePlayerStatAction : CustomAction
+{
+    public string StatName;
+    public StatEvaluation EvaluationType;
+    public int TestValue;
+    public CustomAction OnTrue;
+    public CustomAction OnFalse;
+
+    public override void Initiate()
+    {
+        bool evalResult = false;
+        int currentStatValue = Service.PlayerData.GetStat(StatName);
+        switch(EvaluationType)
+        {
+            case StatEvaluation.GreaterThan:
+                evalResult = currentStatValue > TestValue;
+                break;
+            case StatEvaluation.LessThan:
+                evalResult = currentStatValue < TestValue;
+                break;
+            case StatEvaluation.EqualTo:
+                evalResult = currentStatValue == TestValue;
+                break;
+            case StatEvaluation.NotEqualTo:
+                evalResult = currentStatValue != TestValue;
+                break;
+        }
+
+        if (evalResult && OnTrue)
+        {
+            OnTrue.Initiate();
+        }
+        else if (!evalResult && OnFalse)
+        {
+            OnFalse.Initiate();
+        }
+    }
+}
