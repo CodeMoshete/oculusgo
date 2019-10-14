392988ce6f4659f33e79bb16258f1fd8ea296649
Ben Mosher <codemoshete@gmail.com>
2019-10-14 01:10:48 -0700

    Testing fixes for orientation on scene load

--- Assets/Source/Monobehaviours/Actions/SetGameObjectRotationAction.cs
+++ Assets/Source/Monobehaviours/Actions/SetGameObjectRotationAction.cs
@@ -4,9 +4,15 @@ public class SetGameObjectRotationAction : CustomAction
 {
     public Transform Target;
     public Vector3 EulerAngles;
+    public CustomAction OnDone;
 
     public override void Initiate()
     {
         Target.eulerAngles = EulerAngles;
+
+        if (OnDone != null)
+        {
+            OnDone.Initiate();
+        }
     }
 }
